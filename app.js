0f45e1ce26b064ef284eb08eb04d29619f466071
Debraj Kundu <debraj.kundu098@gmail.com>
2023-06-14 17:43:06 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+alert("lets see Debraj Kundu portfolio");
+const openMenu = document.querySelector('.open');
+const closeMenu =document.querySelector('.close');
+const navbar = document.querySelector('nav ul');
+
+openMenu.addEventListener('click',()=>{
+    navbar.classList.add('active');
+})
+closeMenu.addEventListener('click',()=>{
+    navbar.classList.remove('active');
+})
+
+
